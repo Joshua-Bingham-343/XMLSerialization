7212dfac9e42d0cfb03014d356f6a589d077de07
Eternal-Havok <bingham343@yahoo.com>
2015-11-11 09:40:17 -0500

    Adding XStream tests, not accepting import

--- src/TestObject.java
+++ src/TestObject.java
@@ -1,4 +1,9 @@
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.XstreamTest;
 
 
 public class TestObject {
@@ -19,6 +24,46 @@ public class TestObject {
 		holder.add(new Integer(ammount));
 		holder.add(new Double(value));
 		holder.add(new Character(designation));
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String n){
+		name = n;
+	}
+	
+	public int getAmmount(){
+		return ammount;
+	}
+	
+	public void setAmmount(int a){
+		ammount = a;
+	}
+	
+	public double getValue(){
+		return value;
+	}
+	
+	public void setValue(double v){
+		value = v;
+	}
+	
+	public char getDesignation(){
+		return designation;
+	}
+	
+	public void setDesignation(char c){
+		designation = c;
+	}
+	
+	public ArrayList<Object> getHolder(){
+		return holder;
+	}
+	
+	public void setHolder(ArrayList<Object> h){
+		holder = h;
 	}
 	
 	public TestObject(){
@@ -44,6 +89,11 @@ public class TestObject {
 		TestObject1 test2Recall = Serializer.read1("TestObjectFile1.xml");
 		System.out.println(testRecall);
 		System.out.println(test2Recall.getName());
+		
+		XstreamTest test3 = new XstreamTest("Sandwhich", "Standard PB&J");
+		BufferedOutputStream write = new BufferedOutputStream(new FileOutputStream("TestObjectFile2.xml"));
+		XStream xstream = new XStream();
+		xstream.toXML(test3,write);
 	}
 
 }
---
+++ src/com/thoughtworks/xstream/XstreamTest.java
@@ -0,0 +1,26 @@
+package com.thoughtworks.xstream;
+public class XstreamTest {
+	private String name;
+	private String description;
+
+	public XstreamTest(String n, String d){
+		name = n;
+		description = d;
+	}
+
+	public void setName(String n){
+		name = n;
+	}
+
+	public String getName(){
+		return name;
+	}
+	
+	public void setDescription(String d){
+		description = d;
+	}
+	
+	public String getDescription(){
+		return description;
+	}
+}
